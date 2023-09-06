d10a4ccd5ee6c7090f5b12bc34d9c13bae5551ee
Eddynah Neymour <111374892+EddynahNeymour@users.noreply.github.com>
2023-09-06 12:19:25 -0400

    Add files via upload

---
+++ rewards-converter-flow/rewards-converter-flow/src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    public static void main(String[] args) {
+
+    private int cash;
+    private int miles;
+
+    public RewardValue(int cash, int miles)
+    {
+        cashValue = cash;
+        worth = miles;
+    }
+
+    public int getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return miles;
+    }
+
+   }
+}
